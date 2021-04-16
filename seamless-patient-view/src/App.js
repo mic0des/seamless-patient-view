16d987c180d256f616b063d0fce32a5e078643fa
mic0des <michaelangelo.yambao@gmail.com>
2021-04-15 20:58:34 -0400

    setup data massager and reducer

--- seamless-patient-view/src/App.js
+++ seamless-patient-view/src/App.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useContaxt } from 'react';
+import React, { useEffect, useContext } from 'react';
 import axios from 'axios';
+import { Context } from './state/Store';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const {state, dispatch} = useContext(Context);
 
   useEffect(() => {
     axios
@@ -11,8 +14,16 @@ function App() {
         'https://hapi.fhir.org/baseR4/Patient?birthdate=1950-01-01&_format=json&_pretty=true'
       )
       .then(function (response) {
+        let massagedData = response.data.entry.map(patient => {
+          let resource = patient.resource;
+          return {
+            name: resource.name[0].text ? resource.name[0].text : resource.name[0].given.reverse().map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' '),
+            gender: resource.gender,
+            birthDate: resource.birthDate
+          }
+        })
         debugger
-        // dispatch({ type: 'SET_DEFAULT_SETTINGS', payload: [response.data] })
+        dispatch({ type: 'SET_DATA', payload: [massagedData] }) 
       })
       .catch((err) => new Error('Error fetching performance trend data', err))
   }, [])
--- seamless-patient-view/src/state/Reducer.js
+++ seamless-patient-view/src/state/Reducer.js
@@ -1,11 +1,13 @@
 const Reducer = (state, action) => {
     switch (action.type) {
-        case 'SET_DATA': {
+        case 'SET_DATA': 
+            debugger
             return {
                 ...state,
                 patientData: action.payload
-            }
-        }
+            };
+        default:
+            return state;
     }
 };
 
