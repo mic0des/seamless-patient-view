2bbe8e86e8d002d062910acfdcac4eadf06cfe15
mic0des <michaelangelo.yambao@gmail.com>
2021-04-21 01:30:59 -0400

    delete old source code

--- seamless-patient-view/src/utils/calculateAge.js
+++
@@ -1,5 +0,0 @@
-export default function calculateAge(dateString) {
-    // new Date("2015-02-06") 
-    // return Date.now() - new Date(dateString) 
-    return Math.floor((Date.now() - new Date("2015-02-06")) / 31536000000)
-}
--- seamless-patient-view/src/utils/formatCase.js
+++
@@ -1,3 +0,0 @@
-export default function formatCase(str) {
-    return str.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' ')
-}
