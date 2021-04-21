2c3125f7e83e6df48f2e43860a5190d746bd8571
mic0des <michaelangelo.yambao@gmail.com>
2021-04-21 02:31:58 -0400

    change tooltip initial position

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -1,6 +1,6 @@
 export default function Tooltip({displayObj}) {
     return (
-        <div className="tooltip" style={{opacity: 1, position: 'absolute', left: `${displayObj ? `${displayObj.dimensions.left + 5.5}em` : '5.5em'}`, top: `${displayObj ? `${displayObj.dimensions.top + 8}em` : '8em'}`}}>
+        <div className="tooltip" style={{opacity: 1, position: 'absolute', left: `${displayObj ? `${displayObj.dimensions.left + 17.5}em` : '17.5em'}`, top: `${displayObj ? `${displayObj.dimensions.top + 8}em` : '8em'}`}}>
             <div className="tooltip-range">
                 Patient Ages
             </div>
